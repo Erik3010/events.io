a4beb3646fed10dcf50252e44ccd36acd066ac06
Erik <erikhau3010@gmail.com>
2021-09-04 00:02:54 +0700

    file structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello World</h1>
     </div>
   );
---
+++ src/components/pages/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default Login;
