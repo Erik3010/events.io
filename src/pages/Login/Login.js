dd7eb4813f660aa0c3b75fc5d7568ea696486cb9
Erik <erikhau3010@gmail.com>
2021-09-06 02:29:45 +0700

    decorate sidebar

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,6 +5,9 @@ import LoginFormHeader from "components/Login/LoginFormHeader/LoginFormHeader";
 import LoginForm from "components/Login/LoginForm/LoginForm";
 import LoginFooter from "components/Login/LoginFooter/LoginFooter";
 
+import { ReactComponent as Banner } from "assets/banner.svg";
+import { ReactComponent as Pattern } from "assets/pattern.svg";
+
 function Login() {
   return (
     <main className={styles["login-container"]}>
@@ -16,7 +19,22 @@ function Login() {
         </div>
         <LoginFooter />
       </div>
-      <div className={styles["login-sidebar"]}></div>
+      <div className={styles["login-sidebar"]}>
+        <div className={styles["login-sidebar-overlay"]}>
+          {/* <Pattern width={800} height={800} viewBox="0 0 1100 1100" /> */}
+        </div>
+        <div className={styles["login-sidebar-wrapper"]}>
+          <Banner width={320} height={320} />
+          <div className={styles["login-sidebar-text"]}>
+            <h1>Join an event with just 1 click.</h1>
+            <p>
+              Come with us and get some excellent experience. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Saepe doloremque, beatae
+              nisi laborum sapiente mollitia quia alias ab? Iure, quas?
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
