473863180ba12fe1b5f8bb3540ee24389f52d9d3
Erik <erikhau3010@gmail.com>
2021-09-06 08:35:55 +0700

    add sidebar

---
+++ src/components/Login/LoginSidebar/LoginSidebar.js
@@ -0,0 +1,27 @@
+import styles from "components/Login/LoginSidebar/LoginSidebar.module.scss";
+
+import { ReactComponent as Banner } from "assets/banner.svg";
+import { ReactComponent as Pattern } from "assets/pattern.svg";
+
+function LoginSidebar() {
+  return (
+    <div className={styles["login-sidebar"]}>
+      <div className={styles["login-sidebar-overlay"]}>
+        {/* <Pattern width={800} height={800} viewBox="0 0 1100 1100" /> */}
+      </div>
+      <div className={styles["login-sidebar-wrapper"]}>
+        <Banner width={320} height={320} />
+        <div className={styles["login-sidebar-text"]}>
+          <h1>Join an event with just 1 click.</h1>
+          <p>
+            Come with us and get some excellent experience. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Saepe doloremque, beatae nisi
+            laborum sapiente mollitia quia alias ab? Iure, quas?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginSidebar;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,9 +4,7 @@ import LoginHeader from "components/Login/LoginHeader/LoginHeader";
 import LoginFormHeader from "components/Login/LoginFormHeader/LoginFormHeader";
 import LoginForm from "components/Login/LoginForm/LoginForm";
 import LoginFooter from "components/Login/LoginFooter/LoginFooter";
-
-import { ReactComponent as Banner } from "assets/banner.svg";
-import { ReactComponent as Pattern } from "assets/pattern.svg";
+import LoginSidebar from "components/Login/LoginSidebar/LoginSidebar";
 
 function Login() {
   return (
@@ -19,22 +17,7 @@ function Login() {
         </div>
         <LoginFooter />
       </div>
-      <div className={styles["login-sidebar"]}>
-        <div className={styles["login-sidebar-overlay"]}>
-          {/* <Pattern width={800} height={800} viewBox="0 0 1100 1100" /> */}
-        </div>
-        <div className={styles["login-sidebar-wrapper"]}>
-          <Banner width={320} height={320} />
-          <div className={styles["login-sidebar-text"]}>
-            <h1>Join an event with just 1 click.</h1>
-            <p>
-              Come with us and get some excellent experience. Lorem ipsum dolor
-              sit amet consectetur adipisicing elit. Saepe doloremque, beatae
-              nisi laborum sapiente mollitia quia alias ab? Iure, quas?
-            </p>
-          </div>
-        </div>
-      </div>
+      <LoginSidebar />
     </main>
   );
 }
