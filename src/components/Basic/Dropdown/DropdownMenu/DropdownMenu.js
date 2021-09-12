c893368379bf0f8596ce1f7b46814cb8a7395224
Erik <erikhau3010@gmail.com>
2021-09-12 16:17:47 +0700

    add transition to dropdown

--- src/components/Basic/Dropdown/DropdownMenu/DropdownMenu.js
+++ src/components/Basic/Dropdown/DropdownMenu/DropdownMenu.js
@@ -4,10 +4,41 @@ import styles from "components/Basic/Dropdown/DropdownMenu/DropdownMenu.module.s
 
 import DropdownContext from "components/Basic/Dropdown/DropdownContext";
 
+import { Transition } from "react-transition-group";
+
+const transitionStyle = {
+  entering: { opacity: 0, transform: "translateY(-50%)" },
+  entered: { opacity: 1, transform: "translateY(0)" },
+  exiting: { opacity: 0, transform: "translateY(-50%)" },
+  exited: { opacity: 0, transform: "translateY(-50%)" },
+};
+
 function DropdownMenu({ children }) {
   const { show } = useContext(DropdownContext);
 
-  return show && <div className={styles["dropdown-menu"]}>{children}</div>;
+  return (
+    <Transition
+      in={show}
+      timeout={{
+        appear: 300,
+        enter: 100,
+        exit: 300,
+      }}
+      appear
+      unmountOnExit
+    >
+      {(state) => {
+        return (
+          <div
+            style={{ ...transitionStyle[state] }}
+            className={styles["dropdown-menu"]}
+          >
+            {children}
+          </div>
+        );
+      }}
+    </Transition>
+  );
 }
 
 export default DropdownMenu;
