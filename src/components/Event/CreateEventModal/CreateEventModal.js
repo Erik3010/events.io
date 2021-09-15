2cdb3da4d51bcd7c1ab1cf05f0ac81661185d7e8
Erik <erikhau3010@gmail.com>
2021-09-16 01:15:20 +0700

    add modal decoration

--- src/components/Event/CreateEventModal/CreateEventModal.js
+++ src/components/Event/CreateEventModal/CreateEventModal.js
@@ -5,6 +5,7 @@ import CreateEventForm from "components/Event/CreateEventForm/CreateEventForm";
 import { ReactComponent as Cancel } from "assets/icons/cancel.svg";
 
 import { Transition } from "react-transition-group";
+import classnames from "classnames";
 
 const transitionStyle = {
   entering: { opacity: 0, transform: "scale(.95)" },
@@ -34,18 +35,27 @@ function CreateEventModal({ show, onClose }) {
             <Cancel width={25} height={25} />
           </div>
           <div className={styles["event-modal"]}>
-            <div className={styles["event-modal-header"]}>
-              <h1 className={styles["event-modal-header-title"]}>
-                Create New Event
-              </h1>
-              <p className={styles["event-modal-header-tagline"]}>
-                It take less than 5 minute to fill out the information that
-                needed to create your own events.
-              </p>
+            <div className={styles["event-modal-decoration"]}></div>
+            <div className={styles["event-modal-content"]}>
+              <div className={styles["event-modal-header"]}>
+                <h1 className={styles["event-modal-header-title"]}>
+                  Create New Event
+                </h1>
+                <p className={styles["event-modal-header-tagline"]}>
+                  It take less than 5 minute to fill out the information that
+                  needed to create your own events.
+                </p>
+              </div>
+              <div className={styles["event-modal-body"]}>
+                <CreateEventForm />
+              </div>
             </div>
-            <div className={styles["event-modal-body"]}>
-              <CreateEventForm />
-            </div>
+            <div
+              className={classnames(
+                styles["event-modal-decoration"],
+                styles["event-modal-decoration-right"]
+              )}
+            ></div>
           </div>
         </div>
       )}
