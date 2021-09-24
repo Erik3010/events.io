2f6c8013fac87ad2211f8b0c59f8a330003e2c7c
Erik <erikhau3010@gmail.com>
2021-09-25 02:16:01 +0700

    feat: added attendee table

--- src/components/Event/EventAttendees/EventAttendees.js
+++ src/components/Event/EventAttendees/EventAttendees.js
@@ -1,7 +1,42 @@
+import Avatar from "components/Basic/Avatar/Avatar";
+import Button from "components/Basic/Button/Button";
 import styles from "components/Event/EventAttendees/EventAttendees.module.scss";
 
 function EventAttendees() {
-  return <div>Event Attendees list</div>;
+  return (
+    <div className={styles["attendee-container"]}>
+      <table className={styles["attendee-table"]}>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Ticket</th>
+            <th>Register At</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles["attendee-user"]}>
+                <Avatar color="accent">RO</Avatar>
+                <div className={styles["attendee-user-content"]}>
+                  <h4>Richard Roe</h4>
+                  <p>richard@gmail.com</p>
+                </div>
+              </div>
+            </td>
+            <td>Early bird</td>
+            <td>{new Date().toLocaleString()}</td>
+            <td>
+              <Button type="danger" small>
+                Kick
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default EventAttendees;
