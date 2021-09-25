40b939318f7a74dc9926cda262e1d9ea7a7fb87d
Erik <erikhau3010@gmail.com>
2021-09-25 23:28:00 +0700

    feat: add channel card

--- src/components/Event/EventTimeline/EventTimeline.js
+++ src/components/Event/EventTimeline/EventTimeline.js
@@ -1,7 +1,33 @@
 import styles from "components/Event/EventTimeline/EventTimeline.module.scss";
 
 function EventTimeline() {
-  return <div>Event Timeline</div>;
+  return (
+    <div className={styles["event-timeline"]}>
+      <div className={styles["channel-sidebar"]}>
+        <div className={styles["channel-list"]}>
+          <div className={styles["channel-card"]}>
+            <div className={styles["channel-card-title"]}>#roadmap</div>
+            <div className={styles["channel-card-subtitle"]}>
+              This is a roadmap of javascript frontend
+            </div>
+          </div>
+          <div className={styles["channel-card"]}>
+            <div className={styles["channel-card-title"]}>#roadmap</div>
+            <div className={styles["channel-card-subtitle"]}>
+              This is a roadmap of javascript frontend
+            </div>
+          </div>
+          <div className={styles["channel-card"]}>
+            <div className={styles["channel-card-title"]}>#roadmap</div>
+            <div className={styles["channel-card-subtitle"]}>
+              This is a roadmap of javascript frontend
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles["room-timeline"]}></div>
+    </div>
+  );
 }
 
 export default EventTimeline;
