6c7ee98c78ba8d4cfe22572e753deb8428d91564
Erik <erikhau3010@gmail.com>
2021-09-25 23:48:16 +0700

    feat: add timeline header

--- src/components/Event/EventTimeline/EventTimeline.js
+++ src/components/Event/EventTimeline/EventTimeline.js
@@ -1,5 +1,7 @@
 import styles from "components/Event/EventTimeline/EventTimeline.module.scss";
 
+import cx from "classnames";
+
 function EventTimeline() {
   return (
     <div className={styles["event-timeline"]}>
@@ -11,21 +13,16 @@ function EventTimeline() {
               This is a roadmap of javascript frontend
             </div>
           </div>
-          <div className={styles["channel-card"]}>
-            <div className={styles["channel-card-title"]}>#roadmap</div>
-            <div className={styles["channel-card-subtitle"]}>
-              This is a roadmap of javascript frontend
-            </div>
-          </div>
-          <div className={styles["channel-card"]}>
-            <div className={styles["channel-card-title"]}>#roadmap</div>
-            <div className={styles["channel-card-subtitle"]}>
-              This is a roadmap of javascript frontend
-            </div>
-          </div>
         </div>
       </div>
-      <div className={styles["room-timeline"]}></div>
+      <div className={styles["timeline-container"]}>
+        <div className={styles["timeline-header"]}>
+          <h1>
+            <span>#</span>roadmap
+          </h1>
+          <p>This is a roadmap of javascript frontend</p>
+        </div>
+      </div>
     </div>
   );
 }
