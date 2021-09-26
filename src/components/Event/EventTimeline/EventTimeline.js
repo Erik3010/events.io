506b835eb2d3427684463b57f09a06f499ebdf09
Erik <erikhau3010@gmail.com>
2021-09-26 22:31:39 +0700

    add timeline

--- src/components/Event/EventTimeline/EventTimeline.js
+++ src/components/Event/EventTimeline/EventTimeline.js
@@ -7,6 +7,12 @@ function EventTimeline() {
     <div className={styles["event-timeline"]}>
       <div className={styles["channel-sidebar"]}>
         <div className={styles["channel-list"]}>
+          <div className={cx([styles["channel-card"], styles["active"]])}>
+            <div className={styles["channel-card-title"]}>#roadmap</div>
+            <div className={styles["channel-card-subtitle"]}>
+              This is a roadmap of javascript frontend
+            </div>
+          </div>
           <div className={styles["channel-card"]}>
             <div className={styles["channel-card-title"]}>#roadmap</div>
             <div className={styles["channel-card-subtitle"]}>
@@ -21,6 +27,52 @@ function EventTimeline() {
             <span>#</span>roadmap
           </h1>
           <p>This is a roadmap of javascript frontend</p>
+        </div>
+        <div className={styles["timeline-content"]}>
+          <div className={styles["timeline-table"]}>
+            <div className={styles["timeline-header"]}>
+              <div
+                className={cx([
+                  styles["timeline-cell"],
+                  styles["timeline-room-header"],
+                ])}
+              ></div>
+              <div className={styles["timeline-cell"]}>09:00</div>
+              <div className={styles["timeline-cell"]}>11:00</div>
+              <div className={styles["timeline-cell"]}>13:00</div>
+              <div className={styles["timeline-cell"]}>15:00</div>
+            </div>
+            <div className={styles["timeline-body"]}>
+              <div className={styles["timeline-row"]}>
+                <div
+                  className={cx([
+                    styles["timeline-cell"],
+                    styles["timeline-room-title"],
+                  ])}
+                >
+                  Main Hall
+                </div>
+                <div className={styles["timeline-cell"]}>Main Hall</div>
+                <div className={styles["timeline-cell"]}>Main Hall</div>
+                <div className={styles["timeline-cell"]}>Main Hall</div>
+                <div className={styles["timeline-cell"]}>Main Hall</div>
+              </div>
+              <div className={styles["timeline-row"]}>
+                <div
+                  className={cx([
+                    styles["timeline-cell"],
+                    styles["timeline-room-title"],
+                  ])}
+                >
+                  Main Hall
+                </div>
+                <div className={styles["timeline-cell"]}>Main Hall</div>
+                <div className={styles["timeline-cell"]}>Main Hall</div>
+                <div className={styles["timeline-cell"]}>Main Hall</div>
+                <div className={styles["timeline-cell"]}>Main Hall</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
