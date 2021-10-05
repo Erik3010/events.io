9b8adae45adabadb60d5bae3090afa669e03947c
Erik <erikhau3010@gmail.com>
2021-10-05 23:27:46 +0700

    feat: add registration header

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,15 +1,33 @@
+import styles from "pages/Registration/Registration.module.scss";
+
 import Default from "containers/Default";
 
 function Registration() {
   return (
     <Default>
       <div>
-        <div className="registration-header">
-          <div className="registration-header-title">
+        <div className={styles["registration-header"]}>
+          <div className={styles["registration-header-title"]}>
             <h1>Vue Conference</h1>
-            <div className="registration-header-subtitle">
-              Order Date <span>{new Date().toLocaleString()}</span>
+            <div className={styles["registration-header-subtitle"]}>
+              <div className={styles["registration-sub-content"]}>
+                Order Date:{" "}
+                <span>
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  }).format(new Date())}
+                </span>
+              </div>
+              <div className={styles["separator"]}>|</div>
+              <div className={styles["registration-sub-content"]}>
+                Location: <span>Zoom (Online Meeting)</span>
+              </div>
             </div>
+          </div>
+          <div className={styles["registration-stepper-container"]}>
+            <div className={styles["registration-stepper"]}></div>
           </div>
         </div>
       </div>
