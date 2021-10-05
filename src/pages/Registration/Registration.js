163a89e9c0113bad0c441c69d7875796d7058bb1
Erik <erikhau3010@gmail.com>
2021-10-06 00:19:00 +0700

    feat: add registration stepper

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -2,6 +2,13 @@ import styles from "pages/Registration/Registration.module.scss";
 
 import Default from "containers/Default";
 
+import cx from "classnames";
+
+import { ReactComponent as User } from "assets/icons/user-solid.svg";
+import { ReactComponent as UserGroup } from "assets/icons/user-group-solid.svg";
+import { ReactComponent as Ticket } from "assets/icons/ticket-solid.svg";
+import { ReactComponent as ShieldCheck } from "assets/icons/shield-check-solid.svg";
+
 function Registration() {
   return (
     <Default>
@@ -26,8 +33,36 @@ function Registration() {
               </div>
             </div>
           </div>
-          <div className={styles["registration-stepper-container"]}>
-            <div className={styles["registration-stepper"]}></div>
+          <div className={styles["registration-stepper"]}>
+            <div className={styles["registration-stepper-item"]}>
+              <div className={styles["registration-stepper-item-icon"]}>
+                <UserGroup width={16} height={16} />
+              </div>
+              <div className={styles["registration-stepper-item-content"]}>
+                <p>Step 1</p>
+                <h3>Attendee Info</h3>
+              </div>
+            </div>
+            <div className={styles["registration-stepper-line"]}></div>
+            <div className={styles["registration-stepper-item"]}>
+              <div className={styles["registration-stepper-item-icon"]}>
+                <Ticket width={16} height={16} />
+              </div>
+              <div className={styles["registration-stepper-item-content"]}>
+                <p>Step 2</p>
+                <h3>Choose Ticket</h3>
+              </div>
+            </div>
+            <div className={styles["registration-stepper-line"]}></div>
+            <div className={styles["registration-stepper-item"]}>
+              <div className={styles["registration-stepper-item-icon"]}>
+                <ShieldCheck width={16} height={16} />
+              </div>
+              <div className={styles["registration-stepper-item-content"]}>
+                <p>Step 3</p>
+                <h3>Confirmation</h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
