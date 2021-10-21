c88b55851a8b3de0b1679e193c6a9914e486d73c
Erik <erikhau3010@gmail.com>
2021-10-21 22:11:27 +0700

    feat(style): add styling to ticket card

--- src/components/Registration/RegistrationTicket/RegistrationTicket.js
+++ src/components/Registration/RegistrationTicket/RegistrationTicket.js
@@ -1,14 +1,91 @@
 import styles from "components/Registration/RegistrationTicket/RegistrationTicket.module.scss";
 
+import cx from "classnames";
+
+import { ReactComponent as Check } from "assets/icons/Check.svg";
+
 function RegistrationTicket() {
   return (
     <div className={styles["registration-ticket-container"]}>
-      <div className={styles["registration-ticket-list"]}>
-        <div className={styles["registration-ticket-item"]}>
-          <div className={styles["registration-ticket-item-title"]}>
+      <div className={cx(styles["registration-ticket-item"], styles["active"])}>
+        <div className={styles["registration-ticket-item-check"]}>
+          <Check width={12} height={12} />
+        </div>
+        <div className={styles["registration-ticket-item-header"]}>
+          <div className={styles["registration-ticket-item-header-title"]}>
             Early Bird
           </div>
-          <div className={styles["registration-ticket-item-content"]}></div>
+          <div className={styles["registration-ticket-item-header-tagline"]}>
+            30 tickets left
+          </div>
+        </div>
+        <div className={styles["registration-ticket-item-content"]}>
+          $100.00
+        </div>
+      </div>
+      <div className={styles["registration-ticket-item"]}>
+        <div className={styles["registration-ticket-item-header"]}>
+          <div className={styles["registration-ticket-item-header-title"]}>
+            Early Bird
+          </div>
+          <div className={styles["registration-ticket-item-header-tagline"]}>
+            30 tickets left
+          </div>
+        </div>
+        <div className={styles["registration-ticket-item-content"]}>
+          $100.00
+        </div>
+      </div>
+      <div className={styles["registration-ticket-item"]}>
+        <div className={styles["registration-ticket-item-header"]}>
+          <div className={styles["registration-ticket-item-header-title"]}>
+            Early Bird
+          </div>
+          <div className={styles["registration-ticket-item-header-tagline"]}>
+            30 tickets left
+          </div>
+        </div>
+        <div className={styles["registration-ticket-item-content"]}>
+          $100.00
+        </div>
+      </div>
+      <div className={styles["registration-ticket-item"]}>
+        <div className={styles["registration-ticket-item-header"]}>
+          <div className={styles["registration-ticket-item-header-title"]}>
+            Early Bird
+          </div>
+          <div className={styles["registration-ticket-item-header-tagline"]}>
+            30 tickets left
+          </div>
+        </div>
+        <div className={styles["registration-ticket-item-content"]}>
+          $100.00
+        </div>
+      </div>
+      <div className={styles["registration-ticket-item"]}>
+        <div className={styles["registration-ticket-item-header"]}>
+          <div className={styles["registration-ticket-item-header-title"]}>
+            Early Bird
+          </div>
+          <div className={styles["registration-ticket-item-header-tagline"]}>
+            30 tickets left
+          </div>
+        </div>
+        <div className={styles["registration-ticket-item-content"]}>
+          $100.00
+        </div>
+      </div>
+      <div className={styles["registration-ticket-item"]}>
+        <div className={styles["registration-ticket-item-header"]}>
+          <div className={styles["registration-ticket-item-header-title"]}>
+            Early Bird
+          </div>
+          <div className={styles["registration-ticket-item-header-tagline"]}>
+            30 tickets left
+          </div>
+        </div>
+        <div className={styles["registration-ticket-item-content"]}>
+          $100.00
         </div>
       </div>
     </div>
