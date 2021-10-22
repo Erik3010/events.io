40ad0177c5da17979ca5b8ff6722fba219af819b
Erik <erikhau3010@gmail.com>
2021-10-22 09:08:28 +0700

    feat: add section title in registration

--- src/components/Registration/RegistrationConfiguration/RegistrationConfiguration.js
+++ src/components/Registration/RegistrationConfiguration/RegistrationConfiguration.js
@@ -6,16 +6,24 @@ import RegistrationTicket from "components/Registration/RegistrationTicket/Regis
 
 function RegistrationConfiguration() {
   return (
-    <div className={styles["registration-section"]}>
-      <div className={styles["registration-section-title"]}>
-        Choose your ticket
-      </div>
-      <div className={styles["registration-ticket-container"]}>
-        {Array(10)
-          .fill("")
-          .map((_, index) => (
-            <RegistrationTicket isActive={index === 1} />
-          ))}
+    <div className={styles["registration-section-container"]}>
+      <div className={styles["registration-section"]}>
+        <div className={styles["registration-section-title"]}>
+          <h1>Get Your Tickets</h1>
+          <p>
+            Choose your ticket in here. The ticket price based on the ticket
+            type you've choosed.
+          </p>
+        </div>
+        <div className={styles["registration-section-content"]}>
+          <div className={styles["registration-ticket-container"]}>
+            {Array(10)
+              .fill("")
+              .map((_, index) => (
+                <RegistrationTicket isActive={index === 1} />
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
