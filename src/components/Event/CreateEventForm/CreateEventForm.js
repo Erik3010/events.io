6584b6236d1ebc6215edaa0099c9e30a8794a5c3
Erik <erikhau3010@gmail.com>
2021-11-27 01:22:55 +0700

    style: fix modal form css class name convention

--- src/components/Event/CreateEventForm/CreateEventForm.js
+++ src/components/Event/CreateEventForm/CreateEventForm.js
@@ -3,26 +3,33 @@ import styles from "components/Event/CreateEventForm/CreateEventForm.module.scss
 import Input from "components/Basic/Input/Input";
 import Button from "components/Basic/Button/Button";
 
+import cx from "classnames";
+
 function CreateEventForm() {
   const submitEventHandler = (e) => {
     e.preventDefault();
   };
 
   return (
-    <div className={styles["create-event-form-container"]}>
-      <form
-        className={styles["create-event-form"]}
-        onSubmit={(e) => submitEventHandler(e)}
+    <form
+      className={cx(styles["form"], styles["form--stacked"])}
+      onSubmit={(e) => submitEventHandler(e)}
+    >
+      <Input type="text" label="Name" />
+      <div
+        className={cx(
+          styles["form__input-group"],
+          styles["form__input-group--inline"]
+        )}
       >
-        <Input type="text" label="Name" />
-        <div className={styles["group-form"]}>
-          <Input type="date" label="Date" />
-          <Input type="time" label="Time" />
-        </div>
-        <Input type="text" label="Location" />
+        <Input type="date" label="Date" />
+        <Input type="time" label="Time" />
+      </div>
+      <Input type="text" label="Location" />
+      <div className="form__actions">
         <Button type="primary">Create Event</Button>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 }
 
