931f3adc74608112c547ed3fad6e5389f4f773eb
Erik <erikhau3010@gmail.com>
2021-12-04 10:54:52 +0700

    style: change event timeline css name

--- src/components/Timeline/TimelineTable/TimelineTable.js
+++ src/components/Timeline/TimelineTable/TimelineTable.js
@@ -7,47 +7,49 @@ import SessionItem from "components/Session/SessionItem/SessionItem";
 function TimelineTable() {
   return (
     <div className={styles["table"]}>
-      <div className={styles["table__header"]}>
+      <div className={styles["table__head"]}>
         <div
           className={cx([
-            styles["table__header-cell"],
-            styles["timeline-room-header"],
+            styles["table__head-cell"],
+            styles["table__cell--fixed-width"],
           ])}
         >
           Room
         </div>
-        <div className={styles["table__header-cell"]}>09:00</div>
-        <div className={styles["table__header-cell"]}>11:00</div>
-        <div className={styles["table__header-cell"]}>13:00</div>
-        <div className={styles["table__header-cell"]}>15:00</div>
+        <div className={styles["table__head-cell"]}>09:00</div>
+        <div className={styles["table__head-cell"]}>11:00</div>
+        <div className={styles["table__head-cell"]}>13:00</div>
+        <div className={styles["table__head-cell"]}>15:00</div>
       </div>
-      <div className={styles["timeline-body"]}>
-        <div className={styles["timeline-row"]}>
+      <div className={styles["table__body"]}>
+        <div className={styles["table__row"]}>
           <div
             className={cx([
-              styles["timeline-cell"],
-              styles["timeline-room-title"],
+              styles["table__cell"],
+              styles["table__cell--fixed-width"],
+              styles["table__cell--label"],
             ])}
           >
             Main Hall
           </div>
-          <div className={styles["timeline-cell"]}>
-            <div className={styles["session-list"]}>
+          <div className={styles["table__cell"]}>
+            <div className={styles["table__cell--content"]}>
               <SessionItem>Vue workshop</SessionItem>
             </div>
           </div>
         </div>
-        <div className={styles["timeline-row"]}>
+        <div className={styles["table__row"]}>
           <div
             className={cx([
-              styles["timeline-cell"],
-              styles["timeline-room-title"],
+              styles["table__cell"],
+              styles["table__cell--fixed-width"],
+              styles["table__cell--label"],
             ])}
           >
             Hall
           </div>
-          <div className={styles["timeline-cell"]}>
-            <div className={styles["session-list"]}>
+          <div className={styles["table__cell"]}>
+            <div className={styles["table__cell--content"]}>
               <SessionItem>Vue workshop</SessionItem>
             </div>
           </div>
