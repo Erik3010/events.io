2add99218c3a4f0906407bc78daed0c12a4358df
Erik <erikhau3010@gmail.com>
2021-12-12 16:12:07 +0700

    refactor: clean up jsx

--- src/components/Registration/RegistrationSummary/RegistrationSummary.js
+++ src/components/Registration/RegistrationSummary/RegistrationSummary.js
@@ -17,9 +17,13 @@ function RegistrationSummaryItemHeading({ children }) {
   return <div className={styles["summary__list-title"]}>{children}</div>;
 }
 
-function RegistrationSummaryItem({ label, children }) {
+function RegistrationSummaryItem({ label, largeFont = false, children }) {
   return (
-    <div className={styles["summary__list-content-item"]}>
+    <div
+      className={cx(styles["summary__list-content-item"], {
+        [styles["summary__list-content-item-gtotal"]]: largeFont,
+      })}
+    >
       <span>{label}</span>
       <span className={styles["summary__list-content-item-value"]}>
         {children}
@@ -45,66 +49,46 @@ function RegistrationSummary() {
     <aside className={styles["summary"]}>
       <RegistrationSummaryHeader />
       <div className={styles["summary__content"]}>
-        <div className={styles["summary__list"]}>
-          <RegistrationSummaryItemHeading>
-            Registration Information
-          </RegistrationSummaryItemHeading>
-          <div className={styles["summary__list-content"]}>
-            <RegistrationSummaryItem label="Date">
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "numeric",
-              }).format(new Date())}
-            </RegistrationSummaryItem>
-            <RegistrationSummaryItem label="Time">
-              10:10
-            </RegistrationSummaryItem>
-            <RegistrationSummaryItem label="Total People">
-              2
-            </RegistrationSummaryItem>
-          </div>
+        <RegistrationSummaryItemHeading>
+          Registration Information
+        </RegistrationSummaryItemHeading>
+        <div className={styles["summary__list-content"]}>
+          <RegistrationSummaryItem label="Date">
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            }).format(new Date())}
+          </RegistrationSummaryItem>
+          <RegistrationSummaryItem label="Time">10:10</RegistrationSummaryItem>
+          <RegistrationSummaryItem label="Total People">
+            2
+          </RegistrationSummaryItem>
         </div>
         <RegistrationSummaryDivider />
-        <div className={styles["summary__list"]}>
-          <RegistrationSummaryItemHeading>
-            Tickets
-          </RegistrationSummaryItemHeading>
-          <div className={styles["summary__list-content"]}>
-            <RegistrationSummaryItem label="1 x Early bid">
-              $10.00
-            </RegistrationSummaryItem>
-            <RegistrationSummaryItem label="2 x Early bid">
-              $20.00
-            </RegistrationSummaryItem>
-          </div>
+        <RegistrationSummaryItemHeading>Tickets</RegistrationSummaryItemHeading>
+        <div className={styles["summary__list-content"]}>
+          <RegistrationSummaryItem label="1 x Early bid">
+            $10.00
+          </RegistrationSummaryItem>
+          <RegistrationSummaryItem label="2 x Early bid">
+            $20.00
+          </RegistrationSummaryItem>
         </div>
         <RegistrationSummaryDivider />
-        <div className={styles["summary__list"]}>
-          <div className={styles["summary__list-content"]}>
-            <RegistrationSummaryItem label="Subtotal">
-              $30.00
-            </RegistrationSummaryItem>
-            <RegistrationSummaryItem label="Tax">
-              <span className="text-error">-$10.00</span>
-            </RegistrationSummaryItem>
-          </div>
+        <div className={styles["summary__list-content"]}>
+          <RegistrationSummaryItem label="Subtotal">
+            $30.00
+          </RegistrationSummaryItem>
+          <RegistrationSummaryItem label="Tax">
+            <span className="text-error">-$10.00</span>
+          </RegistrationSummaryItem>
         </div>
         <RegistrationSummaryDivider />
-        <div className={styles["summary__list"]}>
-          <div className={styles["summary__list-content"]}>
-            <div
-              className={cx(
-                styles["summary__list-content-item"],
-                styles["summary__list-content-item-gtotal"]
-              )}
-            >
-              <span>Grandtotal</span>
-              <span className={styles["summary__list-content-item-value"]}>
-                $25.00
-              </span>
-            </div>
-          </div>
+        <div className={styles["summary__list-content"]}>
+          <RegistrationSummaryItem label="Grandtotal" largeFont={true}>
+            $25.00
+          </RegistrationSummaryItem>
         </div>
       </div>
       <RegistrationSummaryFooter />
