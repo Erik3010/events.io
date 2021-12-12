58e7e698a00238e72f511932e30f0465e5cb7f05
Erik <erikhau3010@gmail.com>
2021-12-12 16:37:39 +0700

    refactor: clean up component

--- src/components/Registration/RegistrationSummary/RegistrationSummary.js
+++ src/components/Registration/RegistrationSummary/RegistrationSummary.js
@@ -7,27 +7,29 @@ import Button from "components/Basic/Button/Button";
 function RegistrationSummaryHeader() {
   return (
     <header className={styles["summary__header"]}>
-      <div className={styles["summary__title"]}>Registration ID</div>
-      <div className={styles["summary__subtitle"]}>#ORD12093819</div>
+      <div className={styles["summary__subtitle"]}>Registration ID</div>
+      <div className={styles["summary__title"]}>#ORD12093819</div>
     </header>
   );
 }
 
 function RegistrationSummaryItemHeading({ children }) {
-  return <div className={styles["summary__list-title"]}>{children}</div>;
+  return <div className={styles["summary__heading"]}>{children}</div>;
+}
+
+function RegistrationSummaryList({ children }) {
+  return <div className={styles["summary__list"]}>{children}</div>;
 }
 
 function RegistrationSummaryItem({ label, largeFont = false, children }) {
   return (
     <div
-      className={cx(styles["summary__list-content-item"], {
-        [styles["summary__list-content-item-gtotal"]]: largeFont,
+      className={cx(styles["summary__row"], {
+        [styles["summary__row--lg-font"]]: largeFont,
       })}
     >
       <span>{label}</span>
-      <span className={styles["summary__list-content-item-value"]}>
-        {children}
-      </span>
+      <span className={styles["summary__row-value"]}>{children}</span>
     </div>
   );
 }
@@ -49,10 +51,11 @@ function RegistrationSummary() {
     <aside className={styles["summary"]}>
       <RegistrationSummaryHeader />
       <div className={styles["summary__content"]}>
+        {/* registration information */}
         <RegistrationSummaryItemHeading>
           Registration Information
         </RegistrationSummaryItemHeading>
-        <div className={styles["summary__list-content"]}>
+        <RegistrationSummaryList>
           <RegistrationSummaryItem label="Date">
             {new Intl.DateTimeFormat("en-US", {
               year: "numeric",
@@ -64,32 +67,36 @@ function RegistrationSummary() {
           <RegistrationSummaryItem label="Total People">
             2
           </RegistrationSummaryItem>
-        </div>
+        </RegistrationSummaryList>
         <RegistrationSummaryDivider />
+
+        {/* tickets */}
         <RegistrationSummaryItemHeading>Tickets</RegistrationSummaryItemHeading>
-        <div className={styles["summary__list-content"]}>
+        <RegistrationSummaryList>
           <RegistrationSummaryItem label="1 x Early bid">
             $10.00
           </RegistrationSummaryItem>
           <RegistrationSummaryItem label="2 x Early bid">
             $20.00
           </RegistrationSummaryItem>
-        </div>
+        </RegistrationSummaryList>
         <RegistrationSummaryDivider />
-        <div className={styles["summary__list-content"]}>
+
+        {/* price */}
+        <RegistrationSummaryList>
           <RegistrationSummaryItem label="Subtotal">
             $30.00
           </RegistrationSummaryItem>
           <RegistrationSummaryItem label="Tax">
             <span className="text-error">-$10.00</span>
           </RegistrationSummaryItem>
-        </div>
+        </RegistrationSummaryList>
         <RegistrationSummaryDivider />
-        <div className={styles["summary__list-content"]}>
-          <RegistrationSummaryItem label="Grandtotal" largeFont={true}>
-            $25.00
-          </RegistrationSummaryItem>
-        </div>
+
+        {/* total */}
+        <RegistrationSummaryItem label="Grandtotal" largeFont={true}>
+          $25.00
+        </RegistrationSummaryItem>
       </div>
       <RegistrationSummaryFooter />
     </aside>
