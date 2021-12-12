87d6c215375b34875f1d38e868ebcbd6d41e9b33
Erik <erikhau3010@gmail.com>
2021-12-12 15:23:30 +0700

    refactor: separate some code into separate components

--- src/components/Registration/RegistrationConfirmation/RegistrationConfirmation.js
+++ src/components/Registration/RegistrationConfirmation/RegistrationConfirmation.js
@@ -1,8 +1,6 @@
 import styles from "components/Registration/RegistrationConfirmation/RegistrationConfirmation.module.scss";
 
-import TicketBadge from "components/Registration/TicketBadge/TicketBadge";
 import RegistrationSection from "components/Registration/RegistrationSection/RegistrationSection";
-
 import AttendeeSummary from "components/Registration/AttendeeSummary/AttendeeSummary";
 import TicketSummary from "components/Registration/TicketSummary/TicketSummary";
 import WorkshopSummary from "components/Registration/WorkshopSummary/WorkshopSummary";
--- src/components/Registration/RegistrationSummary/RegistrationSummary.js
+++ src/components/Registration/RegistrationSummary/RegistrationSummary.js
@@ -4,30 +4,36 @@ import cx from "classnames";
 
 import Button from "components/Basic/Button/Button";
 
+function RegistrationSummaryHeader() {
+  return (
+    <header className={styles["summary__header"]}>
+      <div className={styles["summary__title"]}>Registration ID</div>
+      <div className={styles["summary__subtitle"]}>#ORD12093819</div>
+    </header>
+  );
+}
+
+function RegistrationSummaryFooter() {
+  return (
+    <div className={styles["summary__footer"]}>
+      <Button type="primary">Proceed Checkout</Button>
+    </div>
+  );
+}
+
 function RegistrationSummary() {
   return (
-    <div className={styles["registration-summary"]}>
-      <div className={styles["registration-summary-title"]}>
-        <div className={styles["registration-summary-title-tagline"]}>
-          Registration ID
-        </div>
-        <div className={styles["registration-summary-title-id"]}>
-          #ORD12093819
-        </div>
-      </div>
-      <div className={styles["registration-summary-content"]}>
-        <div className={styles["registration-summary-list"]}>
-          <div className={styles["registration-summary-list-title"]}>
+    <aside className={styles["summary"]}>
+      <RegistrationSummaryHeader />
+      <div className={styles["summary-content"]}>
+        <div className={styles["summary-list"]}>
+          <div className={styles["summary-list-title"]}>
             Registration Information
           </div>
-          <div className={styles["registration-summary-list-content"]}>
-            <div className={styles["registration-summary-list-content-item"]}>
+          <div className={styles["summary-list-content"]}>
+            <div className={styles["summary-list-content-item"]}>
               <span>Date</span>
-              <span
-                className={
-                  styles["registration-summary-list-content-item-value"]
-                }
-              >
+              <span className={styles["summary-list-content-item-value"]}>
                 {new Intl.DateTimeFormat("en-US", {
                   year: "numeric",
                   month: "short",
@@ -35,78 +41,56 @@ function RegistrationSummary() {
                 }).format(new Date())}
               </span>
             </div>
-            <div className={styles["registration-summary-list-content-item"]}>
+            <div className={styles["summary-list-content-item"]}>
               <span>Time</span>
-              <span
-                className={
-                  styles["registration-summary-list-content-item-value"]
-                }
-              >
+              <span className={styles["summary-list-content-item-value"]}>
                 10:10
               </span>
             </div>
-            <div className={styles["registration-summary-list-content-item"]}>
+            <div className={styles["summary-list-content-item"]}>
               <span>Total People</span>
-              <span
-                className={
-                  styles["registration-summary-list-content-item-value"]
-                }
-              >
+              <span className={styles["summary-list-content-item-value"]}>
                 2
               </span>
             </div>
           </div>
         </div>
-        <div className={styles["registration-summary-divider"]}></div>
-        <div className={styles["registration-summary-list"]}>
-          <div className={styles["registration-summary-list-title"]}>
-            Tickets
-          </div>
-          <div className={styles["registration-summary-list-content"]}>
-            <div className={styles["registration-summary-list-content-item"]}>
+        <div className={styles["summary-divider"]}></div>
+        <div className={styles["summary-list"]}>
+          <div className={styles["summary-list-title"]}>Tickets</div>
+          <div className={styles["summary-list-content"]}>
+            <div className={styles["summary-list-content-item"]}>
               <span>
                 1 <span className={styles["times"]}>x</span> Early bid
               </span>
-              <span
-                className={
-                  styles["registration-summary-list-content-item-value"]
-                }
-              >
+              <span className={styles["summary-list-content-item-value"]}>
                 $10.00
               </span>
             </div>
-            <div className={styles["registration-summary-list-content-item"]}>
+            <div className={styles["summary-list-content-item"]}>
               <span>
                 2 <span className={styles["times"]}>x</span> Early bid
               </span>
-              <span
-                className={
-                  styles["registration-summary-list-content-item-value"]
-                }
-              >
+              <span className={styles["summary-list-content-item-value"]}>
                 $20.00
               </span>
             </div>
           </div>
         </div>
-        <div className={styles["registration-summary-divider"]}></div>
-        <div className={styles["registration-summary-list"]}>
-          <div className={styles["registration-summary-list-content"]}>
-            <div className={styles["registration-summary-list-content-item"]}>
+        <div className={styles["summary-divider"]}></div>
+        <div className={styles["summary-list"]}>
+          <div className={styles["summary-list-content"]}>
+            <div className={styles["summary-list-content-item"]}>
               <span>Subtotal</span>
-              <span
-                className={
-                  styles["registration-summary-list-content-item-value"]
-                }
-              >
+              <span className={styles["summary-list-content-item-value"]}>
                 $30.00
               </span>
             </div>
-            <div className={styles["registration-summary-list-content-item"]}>
+            <div className={styles["summary-list-content-item"]}>
               <span>Fee</span>
               <span
                 className={cx(
-                  styles["registration-summary-list-content-item-value"],
+                  styles["summary-list-content-item-value"],
                   "text-error"
                 )}
               >
@@ -115,31 +99,25 @@ function RegistrationSummary() {
             </div>
           </div>
         </div>
-        <div className={styles["registration-summary-divider"]}></div>
-        <div className={styles["registration-summary-list"]}>
-          <div className={styles["registration-summary-list-content"]}>
+        <div className={styles["summary-divider"]}></div>
+        <div className={styles["summary-list"]}>
+          <div className={styles["summary-list-content"]}>
             <div
               className={cx(
-                styles["registration-summary-list-content-item"],
-                styles["registration-summary-list-content-item-gtotal"]
+                styles["summary-list-content-item"],
+                styles["summary-list-content-item-gtotal"]
               )}
             >
               <span>Grandtotal</span>
-              <span
-                className={
-                  styles["registration-summary-list-content-item-value"]
-                }
-              >
+              <span className={styles["summary-list-content-item-value"]}>
                 $25.00
               </span>
             </div>
           </div>
         </div>
       </div>
-      <div className={styles["registration-summary-footer"]}>
-        <Button type="primary">Proceed Checkout</Button>
-      </div>
-    </div>
+      <RegistrationSummaryFooter />
+    </aside>
   );
 }
 
