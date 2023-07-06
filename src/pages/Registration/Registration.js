c4efbee1c28eddb296d660ecf942f9ec4e13b136
Erik <erikhau3010@gmail.com>
2023-07-06 15:32:19 +0700

    feat: registration page can move to next step

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -15,8 +15,40 @@ import RegistrationConfirmation from "components/Registration/RegistrationConfir
 
 import AttendeeForm from "components/Registration/AttendeeForm/AttendeeForm";
 import RegistrationConfiguration from "components/Registration/RegistrationConfiguration/RegistrationConfiguration";
+import { useState, useMemo, useCallback } from "react";
+
+const registrationStepComponent = {
+  attendeeInfo: AttendeeForm,
+  configuration: RegistrationConfiguration,
+  confirmation: RegistrationConfirmation,
+};
 
 function Registration() {
+  const [step, setStep] = useState(0);
+  const StepComponent = useMemo(() => {
+    const availableSteps = Object.keys(registrationStepComponent);
+    const currentStep = availableSteps[step];
+
+    return registrationStepComponent[currentStep];
+  }, [step]);
+
+  const nextStep = useCallback(
+    (increment = 1) =>
+      () => {
+        const nextIndex = step + increment;
+
+        if (
+          nextIndex < 0 ||
+          nextIndex > Object.keys(registrationStepComponent).length - 1
+        ) {
+          return;
+        }
+
+        setStep(nextIndex);
+      },
+    [step]
+  );
+
   return (
     <Default>
       <div className={styles["registration"]}>
@@ -27,20 +59,21 @@ function Registration() {
               icon={<UserGroup width={16} height={16} />}
               stepCount={1}
               title="Attendee Info"
+              disabled={step !== 0}
               hasLine={true}
             />
             <RegistrationStepperItem
               icon={<Ticket width={16} height={16} />}
               stepCount={2}
               title="Event Configuration"
-              disabled={true}
+              disabled={step !== 1}
               hasLine={true}
             />
             <RegistrationStepperItem
               icon={<ShieldCheck width={16} height={16} />}
               stepCount={3}
               title="Confirmation"
-              disabled={true}
+              disabled={step !== 2}
             />
           </div>
         </header>
@@ -54,16 +87,21 @@ function Registration() {
               event organizer
             </RegistrationCardHeader>
             <div className={styles["card__body"]}>
-              <AttendeeForm />
+              <StepComponent />
+              {/* <AttendeeForm /> */}
               {/* <RegistrationConfiguration /> */}
               {/* <RegistrationConfirmation /> */}
             </div>
             <div className={styles["card__actions"]}>
               <div>
-                <Button type="secondary">Back</Button>
+                <Button type="secondary" onClick={nextStep(-1)}>
+                  Back
+                </Button>
               </div>
               <div className={styles["card__actions--right"]}>
-                <Button type="primary">Next</Button>
+                <Button type="primary" onClick={nextStep()}>
+                  Next
+                </Button>
               </div>
             </div>
           </div>
